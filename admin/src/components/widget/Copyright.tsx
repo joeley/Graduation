93c035e1f7bcd255d671357d17619823c2794a62
Joe <joeley@126.com>
2021-02-25 20:41:58 +0800

    管理后台初始化

---
+++ admin/src/components/widget/Copyright.tsx
@@ -0,0 +1,19 @@
+/*
+ * File: Copyright.tsx
+ * Desc: 版权信息
+ * File Created: 2020-04-12 22:50:33
+ * Author: chenghao
+ * ------
+ * Copyright 2020 - present, karakal
+ */
+import React from 'react';
+
+const Copyright = () => {
+    return (
+        <div>
+            joeley ©{new Date().getFullYear()} Created by joeley@qq.com
+        </div>
+    );
+};
+
+export default Copyright;
